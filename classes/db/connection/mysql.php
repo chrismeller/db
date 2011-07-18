bac2c88987331d6e82a66cebcc6b9a2576b89aae
Chris Meller <chris@doesnthaveone.com>
2011-07-18 13:02:02 -0400

    Only run MySQL connection profiling if profiling is enabled in the config.

--- classes/db/connection/mysql.php
+++ classes/db/connection/mysql.php
@@ -13,7 +13,9 @@
 			// support multiple concurrent unbuffered queries - we use this so we can actually use transactions
 			$attrs[ PDO::MYSQL_ATTR_USE_BUFFERED_QUERY ] = true;
 			
-			$benchmark = Profiler::start('Database', 'mysql-connect');
+			if ( $this->profile ) {
+				$benchmark = Profiler::start('Database', 'mysql-connect');
+			}
 			
 			try {
 				parent::connect( $environment, $config, $attrs );
