8283db243dc7a15594cc631f79370e87fb34a452
Chris Meller <chris@doesnthaveone.com>
2011-07-28 11:13:39 -0400

    Add another sql_t regex operation to handle the mysql format LIMIT #, # where you specify an offset and a number of records.

--- classes/db/connection/odbc/db2.php
+++ classes/db/connection/odbc/db2.php
@@ -4,7 +4,22 @@
 		
 		public function sql_t ( $query ) {
 			
+			// if there's an offset, we need a callback to replace it
+			$query = preg_replace_callback( '/^(.*)LIMIT\s+(\d+),\s+(\d+)$/is', array( $this, 'replace_offset' ), $query );
+			
+			// if there's no offset, it's a simple replace
 			$query = preg_replace( '/LIMIT\s+(\d+)/ims', 'FETCH FIRST ${1} ROWS ONLY', $query );
+			
+			return $query;
+			
+		}
+		
+		public function replace_offset ( $matches ) {
+			
+			$start = $matches[2];
+			$stop = $start + $matches[3];
+			
+			$query = 'select * from ( select OFFSET_TEMP1.*, rownumber() OVER() as ROW_NUM FROM ( ' . $matches[1] . ' ) AS OFFSET_TEMP1 ) AS OFFSET_TEMP2 where ROW_NUM between ' . $start . ' and ' . $stop;
 			
 			return $query;
 			
